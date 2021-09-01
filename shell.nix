4de170c7a1102da6c96c219b9d1e04cc4b710b21
Philippe Camacho <philippe@translucence.net>
2021-09-01 16:26:54 -0400

    Run local geth version for now.

--- shell.nix
+++ shell.nix
@@ -1,19 +1,21 @@
 with import ./nix/nixpkgs.nix {};
 
-let
-  geth = go-ethereum.overrideAttrs (old: rec {
-     src = fetchgit {
-        url = "https://github.com/philippecamacho/go-ethereum/";
-        rev = "ddf77c130afd14a5cabe63c5ed7ed1c56cb5aeb8";
-        sha256 = "05riajfg5wcc40g95r6bdwfya4a0lwlricv6gp6b9wisn6wkphmw";
-  };
-});
+#let
+#  geth = go-ethereum.overrideAttrs (old: rec {
+#     src = fetchgit {
+#        url = "https://gitlab.com/translucence/go-ethereum";
+#        rev = "ddf77c130afd14a5cabe63c5ed7ed1c56cb5aeb8";
+#        sha256 = "05riajfg5wcc40g95r6bdwfya4a0lwlricv6gp6b9wisn6wkphmw";
+#  };
+#
+#  buildPhase = ''
+#            '';
+#});
 
-in mkShell {
+mkShell {
   buildInputs = [
     go            # go language
     solc          # solidity compiler
-    geth          # blockchain node
     hivemind      # process runner
     nodejs-12_x   # nodejs
   ];

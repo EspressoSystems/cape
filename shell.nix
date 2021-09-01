3afd24804f84c0384f54946f2fbc9d0bf787fdcb
Philippe Camacho <philippe@translucence.net>
2021-09-01 12:36:47 -0400

    Trying to download/build the go-ethereum fork using nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,21 @@
 with import ./nix/nixpkgs.nix {};
 
-mkShell {
+let
+  geth = go-ethereum.overrideAttrs (old: rec {
+     src = fetchgit {
+        url = "https://github.com/philippecamacho/go-ethereum/";
+        rev = "ddf77c130afd14a5cabe63c5ed7ed1c56cb5aeb8";
+        sha256 = "05riajfg5wcc40g95r6bdwfya4a0lwlricv6gp6b9wisn6wkphmw";
+  };
+});
+
+in mkShell {
   buildInputs = [
+    go            # go language
     solc          # solidity compiler
-    go-ethereum   # blockchain node
+    geth          # blockchain node
     hivemind      # process runner
+    nodejs-12_x   # nodejs
   ];
 
   shellHook = ''

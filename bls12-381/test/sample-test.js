9c32acb52f9150a212b49ac94d70a87225f412e5
Philippe Camacho <philippe@translucence.net>
2021-09-03 20:35:53 -0400

    Wrapper for calling precompiled contract

--- bls12-381/test/sample-test.js
+++ bls12-381/test/sample-test.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
+
   it("Should return the new greeting once it's changed", async function () {
 
     const [owner] = await ethers.getSigners();
@@ -17,6 +18,24 @@ describe("Greeter", function () {
     // wait until the transaction is mined
     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    let greeting_str = await greeter.greet();
+    expect(greeting_str).to.equal("Hola, mundo!");
+
   });
+
+  it("Should add two points in G1 correctly", async function () {
+
+    const [owner] = await ethers.getSigners();
+
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    // Call BLS12-381 functions
+    let point = await greeter.addG1();
+    console.log(point);
+
+
+  });
+
 });

fceb491a172bbd33fac110380fc72978f3dd31d7
Philippe Camacho <philippe@translucence.net>
2021-09-15 16:26:12 -0300

    Dummy tests for BN254

---
+++ contracts/testBN254.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.7.0;
+
+import { Curve } from "./BN254.sol";
+
+
+contract testBN254  {
+    constructor() public {}
+
+    function g1Add() external view returns (bool) {
+        return true;
+    }
+}
+

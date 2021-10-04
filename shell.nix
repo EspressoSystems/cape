9a64c70282c6a4ba0b004a05df2feb3a409e6edd
sveitser <sveitser@gmail.com>
2021-10-04 18:19:18 +0800

    [#26] Add repo wide formatter with pre commit hook
    - Add treefmt with configuration for rust, js, solidity, python.
    - Add pre-commit hook managed by nix.
    - Import rust shell.nix into repo shell.nix. This removes the need to
      activate another nix shell when working with rust.
    - Remove obsolete formatters.

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 with import ./nix/nixpkgs.nix { };
 
 let
+  pre-commit-check = callPackage ./nix/precommit.nix { };
   mySolc = callPackage ./nix/solc-bin { version = "0.8.4"; };
   myPython = [
     poetry
@@ -8,6 +9,7 @@ let
       projectDir = ./.;
     })
   ];
+  myRustShell = import ./rust/shell.nix { inherit pkgs; };
 in
 mkShell
 {
@@ -19,12 +21,19 @@ mkShell
     nodejs-12_x # nodejs
     jq
     entr # watch files for changes, for example: ls contracts/*.sol | entr -c hardhat compile
-  ] ++ myPython;
+    treefmt # multi language formatter
+  ]
+  ++ myPython
+  ++ myRustShell.buildInputs;
 
   shellHook = ''
     export TEST_MNEMONIC="test test test test test test test test test test test junk"
     export SOLC_VERSION=${mySolc.version}
     export SOLC_PATH=${mySolc}/bin/solc
     export PATH=$(pwd)/bin:$(pwd)/node_modules/.bin:$PATH
-  '';
+
+    # install pre-commit hooks
+    ${pre-commit-check.shellHook}
+  ''
+  + myRustShell.shellHook;
 }

d2e27d7401cf9823273c211678e03cbb7218a9fe
Philippe Camacho <philippe@translucence.net>
2021-10-13 12:37:13 -0300

    Have the SetMerkleTree.sol contract compile.

--- contracts/SetMerkleTree.sol
+++ contracts/SetMerkleTree.sol
@@ -8,7 +8,7 @@ contract SetMerkleTree {
 
     // DeclarationError: Constants of non-value type not yet implemented.
     bytes64 EMPTY_HASH = bytes64(0, 0);
-    bytes64 EMPTY_SUBTREE = "1";
+    bytes64 EMPTY_SUBTREE = EMPTY_HASH; // TODO is this correct?
 
     uint256 constant N = 512;
 
@@ -17,64 +17,72 @@ contract SetMerkleTree {
         bytes32 lo;
     }
 
+    // TODO export this function to some "utils" library ?
+    function are_equal_bytes64(bytes64 memory x, bytes64 memory y)
+        private
+        returns (bool)
+    {
+        return (x.lo == y.lo) && (x.hi == y.hi);
+    }
+
     function check(bytes64[] calldata proof, bytes32 elem)
         public
         view
         returns (bool)
     {
-        if (proof.length == 0) {
-            revert("Proof has length zero");
-        }
-
-        bytes64 memory running_hash = proof[0]; // or -1?
-
-        bytes64 memory h = elem_hash(elem);
-        // bool[] elem_bit_vec = to_bits(elem_hash); // TODO to_bits
-
-        // the path only goes until a terminal node is reached, so skip
-        // part of the bit-vec
-        // uint256 start_bit = elem_bit_vec.length - proof.length;
-        uint256 start_bit = 256 - proof.length;
-
-        // for (uint256 i = start_bit; i < elem_bit_vec.length; i++) {
-        for (uint256 i = start_bit; i < 256; i++) {
-            console.log(i);
-            bytes32 sib = proof[i - start_bit];
-            // TODO all bits
-            bool sib_is_left = (uint256(h.hi) >> i) % 2 == 1;
-
-            bytes32 l;
-            bytes32 r;
-
-            if (sib_is_left) {
-                l = sib;
-                r = running_hash;
-            } else {
-                l = running_hash;
-                r = sib;
-            }
-            running_hash = branch_hash(l, r);
-        }
-
-        bytes64 memory terminal_node = proof[proof.length - 1];
-
-        if (isEqualToRoot(running_hash)) {
-            if (isEmptySubtree(terminal_node)) {
-                return false;
-            } else if (isLeafNode(terminal_node)) {
-                // TODO Need to have the value to compare it.
-                // return terminal_node = elem;
-                return true;
-            } else {
-                revert("Wrong type of terminal node");
-            }
-        } else {
-            // console.log("Running Hash:");
-            // console.logBytes32(running_hash);
-            // console.log("root");
-            // console.logBytes32(root);
-            revert("Hash mismatch");
-        }
+        //        if (proof.length == 0) {
+        //            revert("Proof has length zero");
+        //        }
+        //
+        //        bytes64 memory running_hash = proof[0]; // or -1?
+        //
+        //        bytes64 memory h = elem_hash(elem);
+        //        // bool[] elem_bit_vec = to_bits(elem_hash); // TODO to_bits
+        //
+        //        // the path only goes until a terminal node is reached, so skip
+        //        // part of the bit-vec
+        //        // uint256 start_bit = elem_bit_vec.length - proof.length;
+        //        uint256 start_bit = 256 - proof.length;
+        //
+        //        // for (uint256 i = start_bit; i < elem_bit_vec.length; i++) {
+        //        for (uint256 i = start_bit; i < 256; i++) {
+        //            console.log(i);
+        //            bytes32 sib = proof[i - start_bit];
+        //            // TODO all bits
+        //            bool sib_is_left = (uint256(h.hi) >> i) % 2 == 1;
+        //
+        //            bytes32 l;
+        //            bytes32 r;
+        //
+        //            if (sib_is_left) {
+        //                l = sib;
+        //                r = running_hash;
+        //            } else {
+        //                l = running_hash;
+        //                r = sib;
+        //            }
+        //            running_hash = branch_hash(l, r);
+        //        }
+        //
+        //        bytes64 memory terminal_node = proof[proof.length - 1];
+        //
+        //        if (isEqualToRoot(running_hash)) {
+        //            if (isEmptySubtree(terminal_node)) {
+        //                return false;
+        //            } else if (isLeafNode(terminal_node)) {
+        //                // TODO Need to have the value to compare it.
+        //                // return terminal_node = elem;
+        //                return true;
+        //            } else {
+        //                revert("Wrong type of terminal node");
+        //            }
+        //        } else {
+        //            // console.log("Running Hash:");
+        //            // console.logBytes32(running_hash);
+        //            // console.log("root");
+        //            // console.logBytes32(root);
+        //            revert("Hash mismatch");
+        //        }
     }
 
     function isEqualToRoot(bytes64 memory running_hash)
@@ -86,12 +94,12 @@ contract SetMerkleTree {
         return false;
     }
 
-    function isEmptySubtree(bytes64 memory node) private pure returns (bool) {
-        return node == EMPTY_SUBTREE;
+    function isEmptySubtree(bytes64 memory node) private returns (bool) {
+        return are_equal_bytes64(node, EMPTY_SUBTREE);
     }
 
-    function isLeafNode(bytes64 memory node) private pure returns (bool) {
-        return node != EMPTY_SUBTREE;
+    function isLeafNode(bytes64 memory node) private returns (bool) {
+        return !are_equal_bytes64(node, EMPTY_SUBTREE);
     }
 
     function elem_hash(bytes32 elem) public pure returns (bytes64 memory) {

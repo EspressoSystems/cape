7c69c764dc4f8da6f8577bbbb40af5f288dc43ee
sveitser <sveitser@gmail.com>
2021-10-18 16:08:21 +0800

    Benchmark nullifier merkle tree gas cost
    Each calls to the blake2 hash function costs about 2M gas this includes
    the overhead of making the call, incl. sending the data.
    
    For doing 10 branch hashes the cost is about 23M.

--- scripts/benchmarks.js
+++ scripts/benchmarks.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const { BigNumber, utils } = ethers;
 const common = require("../lib/common");
 
 const N_AAPTX = 1;
@@ -6,13 +7,8 @@ const N_AAPTX = 1;
 const GAS_PRICE = 223; // See https://legacy.ethgasstation.info/calculatorTxV.php
 const ETH_PRICE_USD = 3388;
 
-async function compute_gas_and_price(
-  fun_to_evaluate,
-  chunk,
-  merkle_trees_update,
-  is_starkware
-) {
-  const tx = await fun_to_evaluate(chunk, merkle_trees_update, is_starkware);
+async function compute_gas_and_price(fun_to_evaluate, ...args) {
+  const tx = await fun_to_evaluate(...args, { gasLimit: 30_000_000 });
   const txReceipt = await tx.wait();
 
   const gasUsed = txReceipt.gasUsed.toString();
@@ -20,28 +16,16 @@ async function compute_gas_and_price(
   return [gasUsed, price];
 }
 
-async function print_report(
-  title,
-  fun_to_eval,
-  fun_names,
-  chunk,
-  merkle_tree_update,
-  is_starkware
-) {
+async function print_report(title, fun_to_eval, fun_names, ...args) {
   console.log("**** " + title + "****");
   for (let i = 0; i < fun_to_eval.length; i++) {
     let res;
     try {
-      res = await compute_gas_and_price(
-        fun_to_eval[i],
-        chunk,
-        merkle_tree_update,
-        is_starkware
-      );
+      res = await compute_gas_and_price(fun_to_eval[i], ...args);
       let gas = res[0] / N_AAPTX;
       let price = res[1] / N_AAPTX;
       console.log(
-        fun_names[i] + ":  " + gas + " gas  ------ " + price + " USD "
+        `${fun_names[i]}:  ${gas} gas  ------ ${price.toFixed(1)} USD`
       );
     } catch (error) {
       console.log(error);
@@ -61,10 +45,10 @@ async function main() {
 
   await dpv.deployed();
 
-  console.log("Contract deployed at address " + dpv.address);
+  console.log(`Contract deployed at address ${dpv.address}`);
 
-  fun_to_eval = [dpv.verify_empty, dpv.verify, dpv.batch_verify];
   fun_names = ["verify_empty", "verify", "batch_verify"];
+  fun_to_eval = fun_names.map((name) => dpv[name]);
 
   const chunk = common.create_chunk(N_AAPTX);
 
@@ -94,6 +78,40 @@ async function main() {
     true,
     false
   );
+
+  const Contract = await ethers.getContractFactory("NullifiersMerkleTree");
+  const contract = await Contract.deploy();
+
+  await contract.deployed();
+
+  console.log(`Contract deployed at address ${contract.address}`);
+
+  fun_names = ["elem_hash", "leaf_hash"];
+  fun_to_eval = fun_names.map((name) => contract[name]);
+
+  const nullifier = utils.randomBytes(32);
+  await print_report("Blake2", fun_to_eval, fun_names, nullifier);
+
+  fun_names = ["branch_hash"];
+  fun_to_eval = fun_names.map((name) => contract[name]);
+  const randHash = () =>
+    Array(8)
+      .fill()
+      .map((_) => BigNumber.from(utils.randomBytes(8)));
+  const left = randHash();
+  const right = randHash();
+
+  await print_report("Blake2", fun_to_eval, fun_names, left, right);
+
+  fun_names = ["terminalNodeValueNonEmpty"];
+  fun_to_eval = fun_names.map((name) => contract[name]);
+  const height = 10;
+  const node = {
+    isEmptySubtree: false,
+    height,
+    elem: nullifier,
+  };
+  await print_report(`Blake2 (height=${height})`, fun_to_eval, fun_names, node);
 }
 
 // We recommend this pattern to be able to use async/await everywhere

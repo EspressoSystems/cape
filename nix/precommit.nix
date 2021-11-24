f9921c3051ec2aa944d4938c8c6d040cc1a3ba89
Mathis Antony <sveitser@gmail.com>
2021-11-24 13:45:24 +0800

    Update nix packages

--- nix/precommit.nix
+++ nix/precommit.nix
@@ -4,8 +4,8 @@ let
   nix-pre-commit-hooks = import (pkgs.fetchFromGitHub {
     owner = "cachix";
     repo = "pre-commit-hooks.nix";
-    rev = "3ed0e618cebc1ff291c27b749cf7568959cac028";
-    sha256 = "0zni3zpz544p7bs7a87wjhd6wb7jmicx0sf2s5nrqapnxa97zcs4";
+    rev = "50cfce93606c020b9e69dce24f039b39c34a4c2d";
+    sha256 = "KZoMUmLgJVYnmohhJ/ENeiH8fCN7rY3VyG/4UpDNEWA=";
   });
 in
 nix-pre-commit-hooks.run {

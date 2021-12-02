ba471d735b6076908bc5939d55bc4416b85dab89
sveitser <sveitser@gmail.com>
2021-12-02 18:25:30 +0800

    Undo accidentally commited debugging changes

--- contracts/rust/src/types.rs
+++ contracts/rust/src/types.rs
@@ -2,27 +2,27 @@ use ethers::prelude::abigen;
 
 abigen!(
     TestBN254,
-    "/home/lulu/r/translucence/aap-on-ethereum/contracts/artifacts/contracts/TestBN254.sol/TestBN254/abi.json",
+    "../artifacts/contracts/TestBN254.sol/TestBN254/abi.json",
     event_derives(serde::Deserialize, serde::Serialize);
 
     TestRecordsMerkleTree,
-    "/home/lulu/r/translucence/aap-on-ethereum/contracts/artifacts/contracts/TestRecordsMerkleTree.sol/TestRecordsMerkleTree/abi.json",
+    "../artifacts/contracts/TestRecordsMerkleTree.sol/TestRecordsMerkleTree/abi.json",
     event_derives(serde::Deserialize, serde::Serialize);
 
     TestTranscript,
-    "/home/lulu/r/translucence/aap-on-ethereum/contracts/artifacts/contracts/TestTranscript.sol/TestTranscript/abi.json",
+    "../artifacts/contracts/TestTranscript.sol/TestTranscript/abi.json",
     event_derives(serde::Deserialize, serde::Serialize);
 
     // Currently will not compile
     // CAPE,
-    // "/home/lulu/r/translucence/aap-on-ethereum/contracts/artifacts/contracts/CAPE.sol/CAPE/abi.json",
+    // "../artifacts/contracts/CAPE.sol/CAPE/abi.json",
     // event_derives(serde::Deserialize, serde::Serialize);
 
     // TestCAPE,
-    // "/home/lulu/r/translucence/aap-on-ethereum/contracts/artifacts/contracts/TestCAPE.sol/TestCAPE/abi.json",
+    // "../artifacts/contracts/TestCAPE.sol/TestCAPE/abi.json",
     // event_derives(serde::Deserialize, serde::Serialize);
 
     Greeter,
-    "/home/lulu/r/translucence/aap-on-ethereum/contracts/artifacts/contracts/Greeter.sol/Greeter/abi.json",
+    "../artifacts/contracts/Greeter.sol/Greeter/abi.json",
     event_derives(serde::Deserialize, serde::Serialize);
 );

551ed7739977c3ef481204ea5b4b64f41f36febe
sveitser <sveitser@gmail.com>
2021-12-02 17:59:42 +0800

    Restore TestCAPE contract

--- contracts/contracts/TestCAPE.sol
+++ contracts/contracts/TestCAPE.sol
@@ -4,16 +4,14 @@ pragma solidity ^0.8.0;
 import "./CAPE.sol";
 
 contract TestCAPE is CAPE {
-    // struct BlockSubmission {
-    //     CapeBlock newBlock;
-    //     uint256[] mtFrontier;
-    //     RecordOpening[] burnedRos;
-    // }
-    // function testSubmitCapeBlock(
-    //     CapeBlock memory newBlock,
-    //     uint256[] memory mtFrontier,
-    //     RecordOpening[] memory burnedRos
-    // ) public returns (BlockSubmission memory) {
-    //     return BlockSubmission(newBlock, mtFrontier, burnedRos);
-    // }
+    function _hasNullifierAlreadyBeenPublished(bytes memory _nullifier)
+        public
+        returns (bool)
+    {
+        return hasNullifierAlreadyBeenPublished(_nullifier);
+    }
+
+    function _insertNullifier(bytes memory _nullifier) public {
+        return insertNullifier(_nullifier);
+    }
 }

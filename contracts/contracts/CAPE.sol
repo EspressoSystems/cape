59e4e401fee7701736534b261e1e92d10bf0f6d8
sveitser <sveitser@gmail.com>
2021-12-03 12:04:39 +0800

    Add types to contract. abigen is happy :confused:

--- contracts/contracts/CAPE.sol
+++ contracts/contracts/CAPE.sol
@@ -15,27 +15,27 @@ contract CAPE is NullifiersStore {
         uint256 dummy;
     }
 
-    // struct MintValidityProof {
-    //     // TODO
-    //     uint256 dummy;
-    // }
+    struct MintValidityProof {
+        // TODO
+        uint256 dummy;
+    }
 
-    //    struct GroupProjective {
-    //        uint256 x;
-    //        uint256 y;
-    //        uint256 t;
-    //        uint256 z;
-    //    }
+    struct GroupProjective {
+        uint256 x;
+        uint256 y;
+        uint256 t;
+        uint256 z;
+    }
 
-    //    struct EncKey {
-    //        GroupProjective key;
-    //    }
+    struct EncKey {
+        GroupProjective key;
+    }
 
-    //    struct AuditMemo {
-    //        // is Ciphertext
-    //        EncKey ephemeral;
-    //        uint256[] data;
-    //    }
+    struct AuditMemo {
+        // is Ciphertext
+        EncKey ephemeral;
+        uint256[] data;
+    }
 
     // XXX This wrapper around the solidity array type is to workaround
     // an issue that causes the ethers abigen to fail on nested structs.
@@ -46,36 +46,36 @@ contract CAPE is NullifiersStore {
     //     uint256[] items;
     // }
 
-    //    struct TransferNote {
-    //        uint256[] inputNullifiers;
-    //        uint256[] outputCommitments;
-    //        TransferValidityProof proof;
-    //        AuditMemo auditMemo;
-    //        AuxInfo auxInfo;
-    //    }
-    //
-    //    struct AuxInfo {
-    //        uint256 merkleRoot;
-    //        uint256 fee;
-    //        uint256 validUntil;
-    //        GroupProjective txnMemoVerKey;
-    //    }
-    //
-    //
-    //    struct UserPubKey {
-    //        GroupProjective address_; // TODO Probably not the right type.
-    //        GroupProjective encKey;
-    //    }
-    //
-    //    struct FreezeNote {
-    //        bool field;
-    //        // TODO
-    //    }
+    struct TransferNote {
+        uint256[] inputNullifiers;
+        uint256[] outputCommitments;
+        TransferValidityProof proof;
+        AuditMemo auditMemo;
+        AuxInfo auxInfo;
+    }
+
+    struct AuxInfo {
+        uint256 merkleRoot;
+        uint256 fee;
+        uint256 validUntil;
+        GroupProjective txnMemoVerKey;
+    }
+
+    struct UserPubKey {
+        GroupProjective address_; // TODO Probably not the right type.
+        GroupProjective encKey;
+    }
+
+    struct FreezeNote {
+        bool field;
+        // TODO
+    }
 
     struct CapeTransaction {
         /// DOC COMMENT IGNORED. Documentation for the field named field.
         // For now we only represent the list of nullifiers of a transactions
-        uint256[] nullifiers;
+        // uint256[] nullifiers; (works)
+        TransferNote note;
     }
 
     struct AssetDefinition {
@@ -84,13 +84,11 @@ contract CAPE is NullifiersStore {
     }
 
     struct AssetPolicy {
-        // TODO
-        bool field;
-        //        EncKey auditorPk;
-        //        EncKey credPk;
-        //        EncKey freezerPk;
-        //        bool[12] revealMap; // ATTRS_LEN (8) + 3 + 1
-        //        uint64 revealThreshold;
+        EncKey auditorPk;
+        EncKey credPk;
+        EncKey freezerPk;
+        bool[12] revealMap; // ATTRS_LEN (8) + 3 + 1
+        uint64 revealThreshold;
     }
 
     struct RecordOpening {
@@ -98,12 +96,12 @@ contract CAPE is NullifiersStore {
         // TODO (Philippe will take care of it)
     }
 
-    //    struct CapeBlock {
-    //        CAPETransaction[] txns;
-    //        // CAPETransaction[] burnTxns; // TODO
-    //        //UserPubKey miner; // TODO
-    //        // uint64 blockHeight; // TODO
-    //    }
+    struct CapeBlock {
+        CapeTransaction[] txns;
+        CapeTransaction[] burnTxns; // TODO
+        UserPubKey miner; // TODO
+        uint64 blockHeight; // TODO
+    }
 
     /// @notice Validate a transaction and if successful apply it.
     /// @dev This is the developer doc for validateAndApply.
@@ -111,13 +109,13 @@ contract CAPE is NullifiersStore {
     function validateAndApply(CapeTransaction[] calldata _block) internal {}
 
     /// Process a transaction in the standard way (not a burn?)
-    /// @param _transaction is a CAPETransaction
+    /// @param _transaction is a CapeTransaction
     function processStandardTransaction(CapeTransaction memory _transaction)
         internal
     {}
 
     /// Process a burn transaction.
-    /// @param _transaction is a CAPETransaction
+    /// @param _transaction is a CapeTransaction
     function processBurnTransaction(CapeTransaction memory _transaction)
         internal
     {}
@@ -155,13 +153,13 @@ contract CAPE is NullifiersStore {
     /// @param mtFrontier latest frontier of the records merkle tree.
     // /// @param burnedRos record opening of the second outputs of the burn transactions. The information contained in these records opening allow the contract to transfer the erc20 tokens.
     function submitCapeBlock(
-        CapeTransaction[] memory newBlock, // TODO try to use a struct instead of an array of transactions
+        CapeBlock memory newBlock,
         uint256[] memory mtFrontier,
         RecordOpening[] memory burnedRos
     ) public {
         // Go through the nullifiers list of each transaction and do the insertion into the Nullifier Store
-        for (uint256 i = 0; i < newBlock.length; i++) {
-            uint256[] memory nullifiers = newBlock[i].nullifiers;
+        for (uint256 i = 0; i < newBlock.txns.length; i++) {
+            uint256[] memory nullifiers = newBlock.txns[i].note.inputNullifiers;
             for (uint256 j = 0; j < nullifiers.length; j++) {
                 insertNullifier(nullifiers[j]);
             }

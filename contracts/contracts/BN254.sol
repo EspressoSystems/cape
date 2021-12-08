c315a3bd60af749c926fe04d732872c157680f4c
Alex Xiong <alex.xiong.tech@gmail.com>
2021-12-08 20:09:16 +0800

    fix: resolve all solhint warning

--- contracts/contracts/BN254.sol
+++ contracts/contracts/BN254.sol
@@ -1,8 +1,6 @@
-//SPDX-License-Identifier: MIT OR Apache-2.0
-// This file is MIT Licensed.
+// SPDX-License-Identifier: MIT
 //
-// From: https://gist.githubusercontent.com/chriseth/f9be9d9391efc5beb9704255a8e2989d/raw/4d0fb90847df1d4e04d507019031888df8372239/snarktest.solidity
-//
+// Based on : https://gist.githubusercontent.com/chriseth/f9be9d9391efc5beb9704255a8e2989d/raw/4d0fb90847df1d4e04d507019031888df8372239/snarktest.solidity
 // Copyright 2017 Christian Reitwiessner
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.

2eef761f3a973dcc4e476d776599c4cefab3a0cd
Philippe Camacho <philippe.camacho@gmail.com>
2022-01-11 10:24:50 -0300

    Make the initial root values explicit in the code. (#274)
    When initialized the root value of the Merkle should not allow any adversary to forge paths. While the previous version of the code was correct, it is better to make the initialization explicit and document the reason behind it.

--- contracts/contracts/RootStore.sol
+++ contracts/contracts/RootStore.sol
@@ -4,12 +4,22 @@ pragma solidity ^0.8.0;
 contract RootStore {
     uint256[] internal _roots;
     uint64 internal _writeHead;
+    uint256 internal constant _EMPTY_NODE_VALUE = 0;
 
     constructor(uint64 nRoots) {
         // Set up the circular buffer for handling the last N roots
         require(nRoots > 1, "A least 2 roots required");
 
         _roots = new uint256[](nRoots);
+
+        // Set all roots to EMPTY_NODE_VALUE.
+        // This value is such that no adversary can extend a branch from this root node.
+        // See proposition 2, page 48 of the AT-Spec document SpectrumXYZ/AT-spec@01f71ce
+
+        for (uint256 i = 0; i < nRoots; i++) {
+            _roots[i] = _EMPTY_NODE_VALUE;
+        }
+
         _writeHead = 1; // The first root value is 0 when the tree is empty
     }
 

3ef7fde775ca59ff0f11f231ed07397ff764d6c2
zhenfei <zhenfei.zhang@hotmail.com>
2022-01-12 09:20:48 -0500

    impl msm

---
+++ contracts/contracts/mocks/TestPlonkVerifier.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BN254} from "../libraries/BN254.sol";
+import {PlonkVerifier} from "../verifier/PlonkVerifier.sol";
+
+contract TestPlonkVerifier {}

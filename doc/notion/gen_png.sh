6a34e5e1c7ba42d5fa81743ab969ee95c11376ac
Philippe Camacho <philippe.camacho@gmail.com>
2022-01-12 13:00:53 -0300

    Sequence diagram for interaction between wallet, EQS, Relayer and OMS (#306)
    * Sequence diagram for interaction between wallet, EQS, Relayer and Owner's Memo Store.
    
    * Build all .puml files
    
    Co-authored-by: sveitser <sveitser@gmail.com>

--- doc/notion/gen_png.sh
+++ doc/notion/gen_png.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 rm -f *.png
-plantuml smart_contract_sequence_diagram.puml
-plantuml smart_contract_class_diagram.puml
-plantuml global_architecture_components_diagram.puml
+for file in *.puml; do
+    echo $file
+    plantuml $file
+done

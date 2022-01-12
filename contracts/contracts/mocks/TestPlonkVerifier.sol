c36a36eef4b260531ddbda7e7d05ea166e655427
zhenfei <zhenfei.zhang@hotmail.com>
2022-01-12 11:15:21 -0500

    impl evaluate vanishing poly

--- contracts/contracts/mocks/TestPlonkVerifier.sol
+++ contracts/contracts/mocks/TestPlonkVerifier.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.8.0;
 
 import {BN254} from "../libraries/BN254.sol";
 import {PlonkVerifier} from "../verifier/PlonkVerifier.sol";
+import {PolynomialEval} from "../libraries/PolynomialEval.sol";
 
-contract TestPlonkVerifier {}
+contract TestPlonkVerifier {
+    function testEvaluateVanishingPoly(PolynomialEval.EvalDomain memory self, uint256 zeta)
+        public
+        pure
+        returns (uint256)
+    {
+        return PolynomialEval.evaluateVanishingPoly(self, zeta);
+    }
+}

7dfda50bbabfa9d0b40d91773f3bed4f08311cbb
Alex Xiong <alex.xiong.tech@gmail.com>
2022-01-14 20:33:37 +0800

    add tests to lin_poly_const

--- contracts/contracts/mocks/TestPlonkVerifier.sol
+++ contracts/contracts/mocks/TestPlonkVerifier.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {BN254} from "../libraries/BN254.sol";
 import {PlonkVerifier as V} from "../verifier/PlonkVerifier.sol";
-import {PolynomialEval} from "../libraries/PolynomialEval.sol";
+import {PolynomialEval as Poly} from "../libraries/PolynomialEval.sol";
 
 contract TestPlonkVerifier is V {
     function computeAlphaPowers(uint256 alpha)
@@ -12,5 +12,28 @@ contract TestPlonkVerifier is V {
         returns (uint256[2] memory alphaPowers)
     {
         return V._computeAlphaPowers(alpha);
+    }
+
+    function computeLinPolyConstantTerm(
+        Poly.EvalDomain memory domain,
+        Challenges memory chal,
+        VerifyingKey memory verifyingKey,
+        uint256[] memory publicInput,
+        PlonkProof memory proof,
+        uint256 vanishEval,
+        uint256 lagrangeOneEval,
+        uint256[2] memory alphaPowers
+    ) public view returns (uint256 res) {
+        return
+            V._computeLinPolyConstantTerm(
+                domain,
+                chal,
+                verifyingKey,
+                publicInput,
+                proof,
+                vanishEval,
+                lagrangeOneEval,
+                alphaPowers
+            );
     }
 }

cc927ba6b27a1ad7854dba027d259b22b7194b6f
sveitser <sveitser@gmail.com>
2022-02-16 12:32:50 +0100

    Run build-abi before emitting paths to watch

--- contracts/rust/build.rs
+++ contracts/rust/build.rs
@@ -2,12 +2,13 @@ use glob::glob;
 use std::{env, process::Command};
 
 fn main() {
+    // Run the build command first so that the ABI files are for the glob expansion below.
+    Command::new("build-abi")
+        .output()
+        .expect("failed to compile contracts");
+
     for entry in glob(&format!("{}/abi/**/*.json", env!("CONTRACTS_DIR"))).unwrap() {
         // run `cargo build -vv` to inspect output
         println!("cargo:rerun-if-changed={}", entry.unwrap().display());
     }
-
-    Command::new("build-abi")
-        .output()
-        .expect("failed to compile contracts");
 }

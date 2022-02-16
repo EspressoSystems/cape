d1573c8f3fa1873f019428f69a84cd922e3b5f5e
Mathis <sveitser@gmail.com>
2022-02-16 18:46:35 +0100

    Update nixpkgs, rust 1.56.1 -> 1.58.1 (#515)

--- flake.nix
+++ flake.nix
@@ -68,7 +68,7 @@
             pythonEnv
           ];
 
-          stableToolchain = pkgs.rust-bin.stable."1.56.1".minimal.override {
+          stableToolchain = pkgs.rust-bin.stable."1.58.1".minimal.override {
             extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
           };
           rustDeps = with pkgs; [

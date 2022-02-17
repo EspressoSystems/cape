4b438b40f696df628a6b384554babeb80176f96a
John D. Corbett <corbett@pictographer.com>
2022-02-16 16:33:15 -0800

    Added command line argument processing and a start at persistence.

--- address_book/src/lib.rs
+++ address_book/src/lib.rs
@@ -2,9 +2,14 @@ use async_std::{
     sync::{Arc, RwLock},
     task::{spawn, JoinHandle},
 };
+use atomic_store::{
+    error::PersistenceError, load_store::BincodeLoadStore, AppendLog, AtomicStore,
+    AtomicStoreLoader,
+};
 use jf_aap::keys::{UserAddress, UserPubKey};
 use jf_aap::Signature;
 use std::collections::HashMap;
+use std::env;
 use tide::{prelude::*, StatusCode};
 
 pub const DEFAULT_PORT: u16 = 50078u16;
@@ -15,19 +20,26 @@ pub struct InsertPubKey {
     pub sig: Signature,
 }
 
+type AddressKeyMap = HashMap<UserAddress, UserPubKey>;
+
 #[derive(Clone, Default)]
 struct ServerState {
-    map: Arc<RwLock<HashMap<UserAddress, UserPubKey>>>,
+    map: Arc<RwLock<AddressKeyMap>>,
 }
 
-pub async fn init_web_server() -> std::io::Result<JoinHandle<std::io::Result<()>>> {
+pub async fn init_web_server(
+    base_url: &str,
+    store: Option<String>,
+) -> std::io::Result<JoinHandle<std::io::Result<()>>> {
+    match store {
+        Some(store) => println!("Got: {}", store),
+        None => println!("Got none"),
+    };
     tide::log::start();
     let mut app = tide::with_state(ServerState::default());
     app.at("/insert_pubkey").post(insert_pubkey);
     app.at("/request_pubkey").post(request_pubkey);
-    let port = std::env::var("PORT").unwrap_or_else(|_| DEFAULT_PORT.to_string());
-    let address = format!("0.0.0.0:{}", port);
-    Ok(spawn(app.listen(address)))
+    Ok(spawn(app.listen(base_url.to_string())))
 }
 
 /// Lookup a user public key from a signed public key address. Fail with
@@ -69,4 +81,48 @@ async fn request_pubkey(
         }
         _ => Ok(tide::Response::new(StatusCode::NotFound)),
     }
+}
+
+fn write_store(address_key_map: BincodeLoadStore<AddressKeyMap>) {
+    let mut test_path = env::current_dir()
+        .map_err(|e| PersistenceError::StdIoDirOpsError { source: e })
+        .expect("Bad cwd");
+    test_path.push("testing_tmp");
+    let mut store_loader =
+        AtomicStoreLoader::create(test_path.as_path(), "append_log_test_empty_iterator")
+            .expect("AtomicStoreLoader::create failed");
+    let mut persisted_thing = AppendLog::create(
+        &mut store_loader,
+        //<BincodeLoadStore<AddressKeyMap>>::default(),
+        address_key_map,
+        "append_thing",
+        1024,
+    )
+    .expect("AppendLog::create failed");
+
+    // TODO write the AddressKeyMap
+    // let _location = persisted_thing
+    //     .store_resource(&address_key_map)
+    //     .expect("store_resource failed");
+}
+
+fn read_store(address_key_map: BincodeLoadStore<AddressKeyMap>) {
+    let mut test_path = env::current_dir()
+        .map_err(|e| PersistenceError::StdIoDirOpsError { source: e })
+        .expect("Bad cwd");
+    test_path.push("testing_tmp");
+    let mut store_loader =
+        AtomicStoreLoader::create(test_path.as_path(), "append_log_test_empty_iterator")
+            .expect("AtomicStoreLoader::create failed");
+
+    let _atomic_store = AtomicStore::open(store_loader).expect("open failed");
+    // let mut persisted_thing = AppendLog::create(
+    //     &mut store_loader,
+    //     <BincodeLoadStore<AddressKeyMap>>::default(),
+    //     "append_thing",
+    //     1024,
+    // )
+    // .expect("AppendLog::create failed");
+
+    // TODO read the AddressKeyMap
 }
--- address_book/src/main.rs
+++ address_book/src/main.rs
@@ -1,11 +1,79 @@
 use address_book::init_web_server;
+use async_std::path::{Path, PathBuf};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Address Book",
+    about = "Server that provides a key/value store mapping user addresses to public keys"
+)]
+struct ServerOpt {
+    /// Whether to load from persisted state. Defaults to true.
+    ///
+    #[structopt(
+        long = "load_from_store",
+        short = "l",
+        parse(try_from_str),
+        default_value = "true"
+    )]
+    load_from_store: bool,
+
+    /// Path to persistence files.
+    ///
+    /// Persistence files will be nested under the specified directory
+    #[structopt(
+        long = "store_path",
+        short = "s",
+        default_value = ""      // See fn default_store_path().
+    )]
+    store_path: String,
+
+    /// Base URL. Defaults to http://0.0.0.0:50078.
+    #[structopt(long = "url", default_value = "http://0.0.0.0:50078")]
+    base_url: String,
+}
+
+/// Returns the project directory.
+fn project_path() -> PathBuf {
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    println!("path {}", path.display());
+    path
+}
+
+/// Returns the default directory to store persistence files.
+fn default_store_path() -> PathBuf {
+    const STORE_DIR: &str = "src/store/address_book";
+    let dir = project_path();
+    [&dir, Path::new(STORE_DIR)].iter().collect()
+}
+
+/// Gets the directory to public key files.
+// TODO !corbett why return a string when you could return a Path
+fn get_store_dir() -> String {
+    let store_path = ServerOpt::from_args().store_path;
+    if store_path.is_empty() {
+        default_store_path()
+            .into_os_string()
+            .into_string()
+            .expect("Error while converting store path to a string")
+    } else {
+        store_path
+    }
+}
 
 /// Run a web server that provides a key/value store mapping user
 /// addresses to public keys.
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
-    init_web_server().await.unwrap_or_else(|err| {
-        panic!("Web server exited with an error: {}", err);
-    });
+    let store = if ServerOpt::from_args().load_from_store {
+        Some(ServerOpt::from_args().store_path)
+    } else {
+        None
+    };
+    init_web_server(&ServerOpt::from_args().base_url, store)
+        .await
+        .unwrap_or_else(|err| {
+            panic!("Web server exited with an error: {}", err);
+        });
     Ok(())
 }

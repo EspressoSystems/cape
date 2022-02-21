fc25a488f7a58bddcb690ce33eb9c63b17b448b1
Joe Doyle <joe@translucence.net>
2022-02-21 10:58:22 -0800

    build.rs: watch .sol files unless CAPE_DONT_WATCH_SOL_FILES is set

--- contracts/rust/build.rs
+++ contracts/rust/build.rs
@@ -5,15 +5,25 @@ fn find_abi_paths() -> glob::Paths {
     glob(&format!("{}/abi/**/*.json", env!("CONTRACTS_DIR"))).unwrap()
 }
 
+fn find_sol_paths() -> glob::Paths {
+    glob(&format!("{}/**/*.sol", env!("CONTRACTS_DIR"))).unwrap()
+}
+
 fn main() {
     // If no abi files exist, generate them. This enables "cargo build" in a fresh repo.
     Command::new("build-abi")
         .output()
         .expect("failed to compile contracts");
 
+    let mut paths: Vec<_> = find_abi_paths().into_iter().collect();
+    if option_env!("CAPE_DONT_WATCH_SOL_FILES").is_none() {
+        paths.extend(find_sol_paths().into_iter());
+    }
+
     // Rerun this script (and recompile crate) if any abi files change.
-    for entry in find_abi_paths() {
+    for entry in paths {
         // run `cargo build -vv` to inspect output
         println!("cargo:rerun-if-changed={}", entry.unwrap().display());
     }
+    println!("cargo:rerun-if-env-changed=CAPE_DONT_WATCH_SOL_FILES");
 }

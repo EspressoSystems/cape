40f3b6c1a9c45182ad1138a32728507db964d540
sveitser <sveitser@gmail.com>
2022-02-21 11:53:13 +0100

    Remove reduntant rerun-if-changed for ABI files
    Now that we include the ABI files with `include_bytes` the crate will
    recompile if the ABI files are modified. Before this change the crate
    recompiles twice each time an ABI file is modified.

--- contracts/rust/build.rs
+++ contracts/rust/build.rs
@@ -1,19 +1,8 @@
-use glob::glob;
-use std::{env, process::Command};
-
-fn find_abi_paths() -> glob::Paths {
-    glob(&format!("{}/abi/**/*.json", env!("CONTRACTS_DIR"))).unwrap()
-}
+use std::process::Command;
 
 fn main() {
-    // If no abi files exist, generate them. This enables "cargo build" in a fresh repo.
+    // Generate the solidity abi files. This enables "cargo build" in a fresh repo.
     Command::new("build-abi")
         .output()
         .expect("failed to compile contracts");
-
-    // Rerun this script (and recompile crate) if any abi files change.
-    for entry in find_abi_paths() {
-        // run `cargo build -vv` to inspect output
-        println!("cargo:rerun-if-changed={}", entry.unwrap().display());
-    }
 }

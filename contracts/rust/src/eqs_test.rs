b705bcc423026672c42be2c0cb8dbfb2622c6597
Kaley Chicoine <kmchicoine@gmail.com>
2022-02-22 08:21:23 -0800

    added listener/submitter for erc_20_tokens_deposited_filter()

--- contracts/rust/src/eqs_test.rs
+++ contracts/rust/src/eqs_test.rs
@@ -1,18 +1,17 @@
 #[cfg(test)]
 mod tests {
     use crate::{
-        assertion::EnsureMined,
         cape::CapeBlock,
         deploy::{deploy_cape_test, deploy_erc20_token},
         ethereum::get_provider,
         ledger::CapeLedger,
-        state::{erc20_asset_description, CapeEvent, Erc20Code, EthereumAddr},
+        state::{erc20_asset_description, Erc20Code, EthereumAddr},
         test_utils::ContractsInfo,
         types::{self as sol, GenericInto, MerkleRootSol},
     };
     use async_std::sync::Mutex;
     use ethers::prelude::{Middleware, U256};
-    use futures::{pin_mut, select, FutureExt};
+    use futures::FutureExt;
     use itertools::Itertools;
     use jf_cap::{
         keys::UserPubKey,
@@ -21,7 +20,7 @@ mod tests {
         },
         utils::TxnsParams,
     };
-    use reef::{traits::Transaction, Ledger};
+    use reef::Ledger;
 
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub(crate) struct CAPEConstructorArgs {
@@ -57,7 +56,6 @@ mod tests {
             CapeLedger::merkle_height(),
         );
         let miner = UserPubKey::default();
-        //let root = params.txns[0].merkle_root();
 
         let cape_block_erc20 = CapeBlock::generate(params_erc20.txns, vec![], miner.address())?;
 
@@ -73,16 +71,14 @@ mod tests {
         let contracts_info = ContractsInfo::new(&cape_contract, &erc20_token_contract).await;
 
         let root = params_empty.txns[0].merkle_root();
-        //add root
 
+        //add root
         (cape_contract)
             .add_root(root.generic_into::<MerkleRootSol>().0)
             .send()
             .await?
             .await?;
         drop(cape_contract);
-
-        //event listener
 
         let block_committed_event_listener = async {
             let mut number_events = 0;
@@ -94,12 +90,11 @@ mod tests {
                     .query_with_meta()
                     .await
                     .unwrap();
-                dbg!(new_entry.clone());
 
                 drop(cape_contract);
 
                 while new_entry.len() > number_events {
-                    dbg!(new_entry[0].clone());
+                    dbg!(new_entry[number_events].clone());
                     //get block from transaction hash
                     let (filter, meta) = new_entry[number_events].clone();
 
@@ -127,17 +122,17 @@ mod tests {
                         })
                         .collect_vec();
                     let input_rc = decoded_cape_block.get_list_of_input_record_commitments();
-                    dbg!(input_rc);
-                    //eqs.handle_event(CapeEvent::BlockCommitted{wraps: wraps, txns: txns});
+                    //dbg!(input_rc);
+                    //TODO: actually use this data output
 
                     number_events += 1;
                 }
             }
         };
 
-        /*let erc20_deposited_event_listener = async {
+        let erc_20_tokens_deposited_event_listener = async {
             let mut number_events = 0;
-            while number_events < 5 {
+            while number_events < 1 {
                 let cape_contract = cape_contract_lock.lock().await;
 
                 let new_erc20_deposit = cape_contract
@@ -152,15 +147,13 @@ mod tests {
                     number_events += 1;
                 }
             }
+        };
 
-        };*/
-
-        //block submitter
         let empty_block_submitter = async {
             let params = vec![];
             let miner = UserPubKey::default();
             let mut blocks_submitted = 0;
-            while blocks_submitted < 5 {
+            while blocks_submitted < 7 {
                 blocks_submitted += 1;
                 let cape_block =
                     CapeBlock::generate(params.clone(), vec![], miner.address()).unwrap();
@@ -176,8 +169,7 @@ mod tests {
             }
         };
 
-        /*let erc_20_deposited_submitter = async {
-
+        let erc_20_tokens_deposited_submitter = async {
             //Approve
             let cape_contract = cape_contract_lock.lock().await;
             let deposited_amount = 1000u64;
@@ -190,21 +182,32 @@ mod tests {
                 .unwrap()
                 .await
                 .unwrap();
-
-            // Call CAPE contract function
+            dbg!("approved");
 
             // Sponsor asset type
-            let rng_sponsor  = &mut ark_std::test_rng();
+            let rng_sponsor = &mut ark_std::test_rng();
             let erc20_code = Erc20Code(EthereumAddr(
                 contracts_info.erc20_token_address.to_fixed_bytes(),
             ));
 
             let sponsor = contracts_info.owner_of_erc20_tokens_client_address;
 
-            let description = erc20_asset_description(&erc20_code, &EthereumAddr(sponsor.to_fixed_bytes()));
+            let description =
+                erc20_asset_description(&erc20_code, &EthereumAddr(sponsor.to_fixed_bytes()));
             let asset_code = AssetCode::new_foreign(&description);
-            let asset_def = AssetDefinition::new(asset_code, AssetPolicy::rand_for_test(rng_sponsor)).unwrap();
+            let asset_def =
+                AssetDefinition::new(asset_code, AssetPolicy::rand_for_test(rng_sponsor)).unwrap();
             let asset_def_sol = asset_def.clone().generic_into::<sol::AssetDefinition>();
+
+            println!("Sponsoring asset");
+            contracts_info
+                .cape_contract_for_erc20_owner
+                .sponsor_cape_asset(contracts_info.erc20_token_address, asset_def_sol)
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap();
 
             let ro = RecordOpening::new(
                 rng,
@@ -227,26 +230,23 @@ mod tests {
                 .await
                 .unwrap();
 
-
             println!("Submitting block");
             // Submit to the contract
-            cape_contract_lock
-                .lock()
-                .await
+            cape_contract
                 .submit_cape_block(cape_block_erc20.into())
                 .gas(U256::from(10_000_000))
                 .send()
                 .await
                 .unwrap()
                 .await
-                .unwrap()
-                .ensure_mined();
-
-
-            //Ok(())
-        };*/
-        //let ((), (), ()) = futures::join!(block_committed_event_listener, erc20_deposited_event_listener, empty_block_submitter);
-        let ((), ()) = futures::join!(block_committed_event_listener, empty_block_submitter);
+                .unwrap();
+        };
+        let ((), (), (), ()) = futures::join!(
+            block_committed_event_listener,
+            erc_20_tokens_deposited_event_listener,
+            erc_20_tokens_deposited_submitter,
+            empty_block_submitter
+        );
         Ok(())
     }
 }

408d972fbf8bb99fb330d9f311b181c7e630341a
sveitser <sveitser@gmail.com>
2022-02-22 17:27:53 +0100

    Add functions to submit block with memos
    - Is this useful?
    - What other utilities are useful?
    - What serialization method should we use?
    - Why can't we return `PendingTransaction` from a function?
    
    The error is
    
        error[E0308]: mismatched types
          --> contracts/rust/src/cape/events.rs:75:8
           |
        75 |     Ok(pending_tx.to_owned())
           |        ^^^^^^^^^^^^^^^^^^^^^ expected struct `PendingTransaction`, found struct `H256
    
    where `pending_tx` is of type `PendingTransaction`.

--- contracts/rust/src/cape/events.rs
+++ contracts/rust/src/cape/events.rs
@@ -1,24 +1,104 @@
+use crate::{cape::CapeBlock, types as sol};
+use anyhow::{Error, Result};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ethers::prelude::{Bytes, Http, Middleware, Provider, TxHash};
+use jf_cap::{structs::ReceiverMemo, Signature};
+
+use crate::{deploy::EthMiddleware, types::TestCAPE};
+
+// XXX Should also allow CAPE, not just TestCAPE
+#[derive(Clone, Debug)]
+pub struct EthConnection {
+    pub contract: TestCAPE<EthMiddleware>,
+    pub provider: Provider<Http>,
+}
+
+impl EthConnection {
+    #[allow(dead_code)]
+    pub fn new(contract: TestCAPE<EthMiddleware>, provider: Provider<Http>) -> Self {
+        Self { contract, provider }
+    }
+}
+
+type BlockMemos = Vec<(Vec<ReceiverMemo>, Signature)>;
+#[derive(Clone, Debug)]
+pub struct BlockWithMemos {
+    pub block: CapeBlock,
+    pub memos: BlockMemos,
+}
+
+impl BlockWithMemos {
+    pub fn new(block: CapeBlock, memos: BlockMemos) -> Self {
+        Self { block, memos }
+    }
+}
+
+#[allow(dead_code)]
+pub async fn fetch_cape_block(
+    connection: &EthConnection,
+    tx_hash: TxHash,
+) -> Result<Option<BlockWithMemos>, Error> {
+    let EthConnection { contract, provider } = connection;
+
+    // Fetch Ethereum transaction that emitted event
+    let tx = if let Some(tx) = provider.get_transaction(tx_hash).await? {
+        tx
+    } else {
+        return Ok(None); // This probably means no tx with this hash found.
+    };
+
+    // Decode the calldata (tx.input) into the function input types
+    let (decoded_calldata_block, fetched_memos_bytes) =
+        contract.decode::<(sol::CapeBlock, Bytes), _>("submitCapeBlockWithMemos", tx.input)?;
+
+    let decoded_cape_block = CapeBlock::from(decoded_calldata_block);
+
+    let decoded_memos: BlockMemos =
+        CanonicalDeserialize::deserialize(&fetched_memos_bytes.to_vec()[..])?;
+
+    Ok(Some(BlockWithMemos::new(decoded_cape_block, decoded_memos)))
+}
+
+#[allow(dead_code)]
+pub async fn submit_cape_block_with_memos(
+    contract: &TestCAPE<EthMiddleware>,
+    block: BlockWithMemos,
+) -> Result<()> {
+    let mut memos_bytes: Vec<u8> = vec![];
+    block.memos.serialize(&mut memos_bytes).unwrap();
+
+    // Submit the block with memos
+    contract
+        .submit_cape_block_with_memos(block.block.clone().into(), memos_bytes.into())
+        .send()
+        .await?
+        .await?;
+
+    // XXX better to return the `PendingTransaction` (before last .await?) but
+    // struggling with type checker if doing so.
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::iter::repeat_with;
 
     use crate::{
-        cape::CapeBlock,
         deploy::deploy_cape_test,
         ethereum::get_provider,
         ledger::CapeLedger,
-        types::{self as sol, GenericInto, MerkleRootSol},
+        types::{GenericInto, MerkleRootSol},
     };
-    use anyhow::Result;
-    use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-    use ethers::prelude::{Bytes, Middleware};
+
     use itertools::Itertools;
+    use jf_cap::KeyPair;
     use jf_cap::{
         keys::{UserKeyPair, UserPubKey},
         sign_receiver_memos,
         structs::{AssetDefinition, FreezeFlag, ReceiverMemo, RecordOpening},
         utils::TxnsParams,
-        KeyPair, Signature,
     };
     use rand::{RngCore, SeedableRng};
     use rand_chacha::ChaChaRng;
@@ -65,17 +145,12 @@ mod tests {
         .take(3)
         .collect_vec();
 
-        let mut memos_bytes: Vec<u8> = vec![];
-        memos_with_sigs.serialize(&mut memos_bytes).unwrap();
+        submit_cape_block_with_memos(
+            &contract,
+            BlockWithMemos::new(cape_block.clone(), memos_with_sigs.clone()),
+        )
+        .await?;
 
-        // Submit the block with memos
-        contract
-            .submit_cape_block_with_memos(cape_block.clone().into(), memos_bytes.into())
-            .send()
-            .await?
-            .await?;
-
-        // Fetch event with metadata
         let events = contract
             .block_committed_filter()
             .from_block(0u64)
@@ -85,24 +160,14 @@ mod tests {
         let (_, meta) = events[0].clone();
 
         let provider = get_provider();
+        let connection = EthConnection::new(contract, provider);
 
-        // Fetch Ethereum transaction that emitted event
-        let tx = provider
-            .get_transaction(meta.transaction_hash)
+        let BlockWithMemos { block, memos } = fetch_cape_block(&connection, meta.transaction_hash)
             .await?
             .unwrap();
 
-        // Decode the calldata (tx.input) into the function input types
-        let (decoded_calldata_block, fetched_memos_bytes) = contract
-            .decode::<(sol::CapeBlock, Bytes), _>("submitCapeBlockWithMemos", tx.input)
-            .unwrap();
-
-        let decoded_cape_block = CapeBlock::from(decoded_calldata_block);
-        assert_eq!(decoded_cape_block, cape_block);
-
-        let decoded_memos: Vec<(Vec<ReceiverMemo>, Signature)> =
-            CanonicalDeserialize::deserialize(&fetched_memos_bytes.to_vec()[..]).unwrap();
-        assert_eq!(decoded_memos, memos_with_sigs);
+        assert_eq!(block, cape_block);
+        assert_eq!(memos, memos_with_sigs);
 
         Ok(())
     }

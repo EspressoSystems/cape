68901a386bcff8f3800ec09ad9c76728cdd51ee5
Alex Xiong <alex.xiong.tech@gmail.com>
2022-02-24 09:05:35 +0800

    add faucet address to deployment scripts (#557)

--- contracts/deploy/00_cape.ts
+++ contracts/deploy/00_cape.ts
@@ -26,10 +26,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const treeDepth = 24;
   const nRoots = 1000;
-  // TODO: (alex) change this to actual wallet's public address
+
+  // Faucet manager wallet public key: USERPUBKEY~uc8DIUZc8j7P01_QiXGYjg_KDDIpuJ1y3bfWjQ3ByyggAAAAAAAAAFezuiSZRdTqt2V8lesbMyY6-QUKuLG-QXIAXZnYUoAbyw
+  // address generation code:
+  // ```rust
+  // use ark_std::str::FromStr;
+  // let result = "USERPUBKEY~uc8DIUZc8j7P01_QiXGYjg_KDDIpuJ1y3bfWjQ3ByyggAAAAAAAAAFezuiSZRdTqt2V8lesbMyY6-QUKuLG-QXIAXZnYUoAbyw";
+  // let pk = UserPubKey::from_str(&result).unwrap_or_default();
+  // ark_std::eprintln!(
+  //     "x: {}, y: {}",
+  //     pk.address().internal().x,
+  //     pk.address().internal().y
+  // );
+  // ```
   const faucetManager = {
-    x: BigNumber.from(0),
-    y: BigNumber.from(1),
+    x: BigNumber.from("0x28CBC10D8DD6B7DD729DB829320CCA0F8E987189D05FD3CF3EF25C462103CFB9"),
+    y: BigNumber.from("0x0B9F465C2530B75A937FDF0B5AD4EF76D0A939E9B5F5F36C4A9BF6279EBC19F0"),
   };
 
   await deploy("CAPE", {

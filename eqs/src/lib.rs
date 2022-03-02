38908d469dbcbced7dc504000885a88ba7d93567
Nathan F Yospe <nyospe@gmail.com>
2022-03-02 12:29:03 -0500

    Incorporate EQSOptions as parameter

--- eqs/src/lib.rs
+++ eqs/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod api_server;
 pub mod configuration;
 pub mod disco; // really needs to go into a shared crate
-pub mod eqs;
+pub mod entry;
 pub mod errors;
 pub mod eth_polling;
 pub mod query_result_state;
@@ -9,4 +9,4 @@ pub mod route_parsing;
 pub mod routes;
 pub mod state_persistence;
 
-pub use crate::eqs::run_eqs;
+pub use crate::entry::run as run_eqs;
--- eqs/src/main.rs
+++ eqs/src/main.rs
@@ -1,16 +1,8 @@
-mod api_server;
-mod configuration;
-mod disco; // really needs to go into a shared crate
-mod eqs;
-mod errors;
-mod eth_polling;
-mod query_result_state;
-mod route_parsing;
-mod routes;
-mod state_persistence;
+use eqs::configuration::EQSOptions;
+use structopt::StructOpt;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt().pretty().init();
-    eqs::run_eqs().await
+    eqs::run_eqs(&EQSOptions::from_args()).await
 }

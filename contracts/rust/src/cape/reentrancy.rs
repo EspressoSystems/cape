172339937016c9dc67bbf82e3d5cbc92902139f6
philippe <philippe.camacho@gmail.com>
2022-03-25 11:08:56 -0300

    Remove superfluous variables.

--- contracts/rust/src/cape/reentrancy.rs
+++ contracts/rust/src/cape/reentrancy.rs
@@ -20,7 +20,6 @@ use std::sync::Arc;
 #[tokio::test]
 async fn test_reentrancy_guard() -> Result<()> {
     let cape_contract = deploy_cape_test().await;
-    let cape_contract_address = cape_contract.address();
     let malicious_erc20_contract = deploy_malicious_erc20_token().await;
     let malicious_erc20_address = malicious_erc20_contract.address();
 
@@ -29,31 +28,34 @@ async fn test_reentrancy_guard() -> Result<()> {
     let erc20_code = Erc20Code(EthereumAddr(malicious_erc20_address.to_fixed_bytes()));
 
     let owner_of_erc20_tokens_client = malicious_erc20_contract.client().clone();
-    let owner_of_erc20_tokens_client_address = owner_of_erc20_tokens_client.address();
 
     let cape_contract_erc20_owner = TestCAPE::new(
         cape_contract.address(),
         Arc::new(owner_of_erc20_tokens_client.clone()),
     );
 
-    let sponsor = owner_of_erc20_tokens_client_address;
+    let sponsor = owner_of_erc20_tokens_client.address();
 
     let description = erc20_asset_description(&erc20_code, &EthereumAddr(sponsor.to_fixed_bytes()));
-    let asset_code = AssetCode::new_foreign(&description);
-    let asset_def = AssetDefinition::new(asset_code, AssetPolicy::rand_for_test(rng)).unwrap();
-    let asset_def_sol = asset_def.clone().generic_into::<sol::AssetDefinition>();
+    let asset_def = AssetDefinition::new(
+        AssetCode::new_foreign(&description),
+        AssetPolicy::rand_for_test(rng),
+    )
+    .unwrap();
 
     // Prepare call to CAPE.deposit
     cape_contract_erc20_owner
-        .sponsor_cape_asset(malicious_erc20_address, asset_def_sol)
+        .sponsor_cape_asset(
+            malicious_erc20_address,
+            asset_def.clone().generic_into::<sol::AssetDefinition>(),
+        )
         .send()
         .await?
         .await?;
 
     let deposited_amount = 1000;
-    let amount_u256 = U256::from(deposited_amount);
     malicious_erc20_contract
-        .approve(cape_contract_address, amount_u256)
+        .approve(cape_contract.address(), U256::from(deposited_amount))
         .send()
         .await?
         .await?;
@@ -68,19 +70,19 @@ async fn test_reentrancy_guard() -> Result<()> {
     );
 
     // By default no error is triggered
-    let call = cape_contract
+    assert!(cape_contract
         .deposit_erc_20(
             ro.clone().generic_into::<sol::RecordOpening>(),
             malicious_erc20_address,
         )
-        .from(owner_of_erc20_tokens_client_address)
+        .from(sponsor)
         .call()
-        .await;
-    assert!(call.is_ok());
+        .await
+        .is_ok());
 
     // Set the CAPE contract address as the target of the malicious contract
     malicious_erc20_contract
-        .set_target_contract_address(cape_contract_address)
+        .set_target_contract_address(cape_contract.address())
         .send()
         .await?
         .await?;
@@ -92,16 +94,15 @@ async fn test_reentrancy_guard() -> Result<()> {
         .await?
         .await?;
 
-    let call = cape_contract
+    cape_contract
         .deposit_erc_20(
             ro.clone().generic_into::<sol::RecordOpening>(),
             malicious_erc20_address,
         )
-        .from(owner_of_erc20_tokens_client_address)
+        .from(sponsor)
         .call()
-        .await;
-
-    call.should_revert_with_message("ReentrancyGuard: reentrant call");
+        .await
+        .should_revert_with_message("ReentrancyGuard: reentrant call");
 
     // Decide to run CAPE.submitBlock when calling MaliciousContract.transferFrom
     malicious_erc20_contract
@@ -110,16 +111,15 @@ async fn test_reentrancy_guard() -> Result<()> {
         .await?
         .await?;
 
-    let call = cape_contract
+    cape_contract
         .deposit_erc_20(
             ro.clone().generic_into::<sol::RecordOpening>(),
             malicious_erc20_address,
         )
-        .from(owner_of_erc20_tokens_client_address)
+        .from(sponsor)
         .call()
-        .await;
-
-    call.should_revert_with_message("ReentrancyGuard: reentrant call");
+        .await
+        .should_revert_with_message("ReentrancyGuard: reentrant call");
 
     Ok(())
 }

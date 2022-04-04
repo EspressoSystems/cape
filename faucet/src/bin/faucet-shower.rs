9ad5a279322b46ab4f2b5e749563c0e2f74d9dff
Jeb Bearer <jeb.bearer@translucence.net>
2022-04-04 06:15:47 -0700

    Remove short options where they are likely to cause conflicts or confusion (#906)

--- faucet/src/bin/faucet-shower.rs
+++ faucet/src/bin/faucet-shower.rs
@@ -42,17 +42,11 @@ pub struct Options {
     pub transfer_size: u64,
 
     /// URL for the Ethereum Query Service.
-    #[structopt(
-        short,
-        long,
-        env = "CAPE_EQS_URL",
-        default_value = "http://localhost:50087"
-    )]
+    #[structopt(long, env = "CAPE_EQS_URL", default_value = "http://localhost:50087")]
     pub eqs_url: Url,
 
     /// URL for the CAPE relayer.
     #[structopt(
-        short,
         long,
         env = "CAPE_RELAYER_URL",
         default_value = "http://localhost:50077"
@@ -61,7 +55,6 @@ pub struct Options {
 
     /// URL for the Ethereum Query Service.
     #[structopt(
-        short,
         long,
         env = "CAPE_ADDRESS_BOOK_URL",
         default_value = "http://localhost:50078"
@@ -69,12 +62,11 @@ pub struct Options {
     pub address_book_url: Url,
 
     /// Address of the CAPE smart contract.
-    #[structopt(short, long, env = "CAPE_CONTRACT_ADDRESS")]
+    #[structopt(long, env = "CAPE_CONTRACT_ADDRESS")]
     pub contract_address: Address,
 
     /// URL for Ethers HTTP Provider
     #[structopt(
-        short,
         long,
         env = "CAPE_WEB3_PROVIDER_URL",
         default_value = "http://localhost:8545"

942bf409969ef99dd2bd2b116efed9b577bb9765
Justin Restivo <justin@restivo.me>
2022-04-15 18:46:39 -0400

    feat: static linking with musl + nix

--- flake.nix
+++ flake.nix
@@ -33,117 +33,69 @@
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
+      xpkgs = import nixpkgs {
+        system = "x86_64-linux"; 
+        overlays = [ (import rust-overlay) ];
       };
+      pkgs = import nixpkgs {
+        localSystem = "x86_64-linux"; 
+        crossSystem = { config = "x86_64-unknown-linux-musl"; };
+        # overlays = [ (import rust-overlay) ];
+      };
+      mySolc = xpkgs.callPackage ./nix/solc-bin { version = "0.8.10"; };
+      pythonEnv = xpkgs.poetry2nix.mkPoetryEnv {
+        projectDir = ./.;
+      };
+      myPython = with xpkgs; [
+        poetry
+        pythonEnv
+      ];
     in
     {
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            lint-solidity = {
-              enable = true;
-              files = "^contracts/contracts/";
-              entry = "lint-solidity";
-              types = [ "solidity" ];
-            };
-            check-format = {
-              enable = true;
-              entry = "treefmt --fail-on-change";
-            };
-            # The hook "clippy" that ships with nix-precommit-hooks is outdated.
-            cargo-clippy = {
-              enable = true;
-              description = "Lint Rust code.";
-              entry = "cargo-clippy --workspace -- -D warnings";
-              files = "\\.rs$";
-              pass_filenames = false;
-            };
-            cargo-sort = {
-              enable = true;
-              description = "Ensure Cargo.toml are sorted";
-              entry = "cargo sort -w";
-              pass_filenames = false;
-            };
-            license-header-c-style = {
-              enable = true;
-              description = "Ensure files with c-style comments have license header";
-              entry = "insert_license --license-filepath .license-header  --comment-style \"//\"";
-              types_or = [ "rust" "ts" "javascript" ];
-              excludes = [
-                "bindings/mod\\.rs" # generated file
-              ];
-              pass_filenames = true;
-            };
-            license-header-solidity = {
-              enable = true;
-              description = "Ensure solidity files have license header";
-              entry = "insert_license --license-filepath .license-header-solidity  --comment-style \"//\"";
-              types = [ "solidity" ];
-              pass_filenames = true;
-            };
-            license-header-hash = {
-              enable = true;
-              description = "Ensure files with hash style comments have license header";
-              entry = "insert_license --license-filepath .license-header --comment-style \"#\"";
-              types_or = [ "bash" "python" "toml" "nix" ];
-              excludes = [
-                "poetry.lock"
-              ];
-              pass_filenames = true;
-            };
-            license-header-html = {
-              enable = true;
-              description = "Ensure markdown files have license header";
-              entry = "insert_license --license-filepath .license-header --comment-style \"<!--| ~| -->\"";
-              types_or = [ "markdown" ];
-              pass_filenames = true;
-            };
-          };
-        };
-      };
       # A nix base image
-      packages.docker = pkgs.dockerTools.buildImage {
-        name = "nix-base-docker";
-        tag = "latest";
-        contents = with pkgs; [
-          curl
-          coreutils
-          bashInteractive
-          cacert
-        ];
-        config = {
-          Env = [
-            "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
-          ];
-        };
-      };
+      # packages.docker = pkgs.dockerTools.buildImage {
+      #   name = "nix-base-docker";
+      #   tag = "latest";
+      #   contents = with pkgs; [
+      #     curl
+      #     coreutils
+      #     bashInteractive
+      #     cacert
+      #   ];
+      #   config = {
+      #     Env = [
+      #       "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
+      #     ];
+      #   };
+      # };
       devShell =
         let
-          mySolc = pkgs.callPackage ./nix/solc-bin { version = "0.8.10"; };
-          pythonEnv = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-          };
-          myPython = with pkgs; [
-            poetry
-            pythonEnv
-          ];
+          # mySolc = pkgs.callPackage ./nix/solc-bin { version = "0.8.10"; };
+          # pythonEnv = pkgs.poetry2nix.mkPoetryEnv {
+          #   projectDir = ./.;
+          # };
+          # myPython = with pkgs; [
+          #   poetry
+          #   pythonEnv
+          # ];
 
-          stableToolchain = pkgs.rust-bin.stable."1.60.0".minimal.override {
+          stableToolchain = xpkgs.rust-bin.stable."1.60.0".minimal.override {
             extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
+	    targets = [ "x86_64-unknown-linux-musl" ];
           };
+          openssl_musl = (((import nixpkgs { localSystem = "x86_64-linux"; crossSystem = { config = "x86_64-unknown-linux-musl"; }; }).openssl).override {static = true;}).dev;
+          # for some odd reason only `out` exposes libssl.a
+          openssl_musl_lib = (((import nixpkgs { localSystem = "x86_64-linux"; crossSystem = { config = "x86_64-unknown-linux-musl"; }; }).openssl).override {static = true;}).out;
           rustDeps = with pkgs; [
             pkgconfig
-            openssl
-
+            openssl_musl
+            openssl_musl_lib
             curl
-            plantuml
+            # plantuml
             stableToolchain
 
-            cargo-edit
-            cargo-sort
+            # cargo-edit
+            # cargo-sort
           ] ++ lib.optionals stdenv.isDarwin [
             # required to compile ethers-rs
             darwin.apple_sdk.frameworks.Security
@@ -152,38 +104,12 @@
             # https://github.com/NixOS/nixpkgs/issues/126182
             libiconv
           ] ++ lib.optionals (stdenv.system != "aarch64-darwin") [
-            cargo-watch # broken: https://github.com/NixOS/nixpkgs/issues/146349
+            # cargo-watch # broken: https://github.com/NixOS/nixpkgs/issues/146349
           ];
           # nixWithFlakes allows pre v2.4 nix installations to use flake commands (like `nix flake update`)
-          nixWithFlakes = pkgs.writeShellScriptBin "nix" ''
-            exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
-          '';
         in
         pkgs.mkShell
           {
-            buildInputs = with pkgs; [
-              nixWithFlakes
-              go-ethereum
-              nodePackages.pnpm
-              mySolc
-              hivemind # process runner
-              nodejs-16_x # nodejs
-              jq
-              entr # watch files for changes, for example: ls contracts/*.sol | entr -c hardhat compile
-              treefmt # multi language formatter
-              nixpkgs-fmt
-              git # required for pre-commit hook installation
-              netcat-gnu # only used to check for open ports
-              cacert
-              mdbook # make-doc, documentation generation
-              moreutils # includes `ts`, used to add timestamps on CI
-            ]
-            ++ myPython
-            ++ rustDeps;
-
-            RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
-            RUST_BACKTRACE = 1;
-            RUST_LOG = "info";
 
             SOLCX_BINARY_PATH = "${mySolc}/bin";
             SOLC_VERSION = mySolc.version;
@@ -191,20 +117,47 @@
             # TODO: increase this when contract size limit is not a problem
             SOLC_OPTIMIZER_RUNS = "20";
 
+            buildInputs = rustDeps ++ [
+            myPython
+              xpkgs.nodePackages.pnpm
+              mySolc
+              xpkgs.hivemind # process runner
+              xpkgs.nodejs-16_x
+              xpkgs.plantuml
+            ];
+
+            # with pkgs; [
+            # stableToolchain
+            #   nixWithFlakes
+            #   go-ethereum
+            #   nodePackages.pnpm
+            #   mySolc
+            #   hivemind # process runner
+            #   nodejs-16_x # nodejs
+            #   jq
+            #   entr # watch files for changes, for example: ls contracts/*.sol | entr -c hardhat compile
+            #   treefmt # multi language formatter
+            #   nixpkgs-fmt
+            #   git # required for pre-commit hook installation
+            #   netcat-gnu # only used to check for open ports
+            #   cacert
+            #   mdbook # make-doc, documentation generation
+            #   moreutils # includes `ts`, used to add timestamps on CI
+            #   # inputs.nixpkgs.legacyPackages.x86_64-musl.openssl
+            # ]
+            # ++ myPython
+            # ++ rustDeps;
+
+            RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
+            RUST_BACKTRACE = 1;
+            RUST_LOG = "info";
+
             shellHook = ''
-              echo "Ensuring node dependencies are installed"
-              pnpm --recursive install
 
-              if [ ! -f .env ]; then
-                echo "Copying .env.sample to .env"
-                cp .env.sample .env
-              fi
-
-              echo "Exporting all vars in .env file"
-              set -a; source .env; set +a;
-
-              # on mac os `bin/pwd -P` returns the canonical path on case insenstive file-systems
               my_pwd=$(/bin/pwd -P 2> /dev/null || pwd)
+
+             set -a; source .env; set +a;
+             pnpm --recursive install
 
               export CONTRACTS_DIR=''${my_pwd}/contracts
               export HARDHAT_CONFIG=$CONTRACTS_DIR/hardhat.config.ts
@@ -213,10 +166,15 @@
               export PATH=''${my_pwd}/bin:$PATH
               export WALLET=''${my_pwd}/wallet
 
-              git config --local blame.ignoreRevsFile .git-blame-ignore-revs
-            ''
-            # install pre-commit hooks
-            + self.checks.${system}.pre-commit-check.shellHook;
+
+
+              export RUSTFLAGS='-C target-feature=+crt-static'
+              export CARGO_BUILD_TARGET='x86_64-unknown-linux-musl'
+              export OPENSSL_LIB_DIR='${openssl_musl}/lib/'
+              export OPENSSL_INCLUDE_DIR='${openssl_musl}/include/'
+              export OPENSSL_STATIC=true
+              export RUSTFLAGS='-L${openssl_musl_lib}/lib/'
+            '';
           };
 
     }

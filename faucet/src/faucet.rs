44d0b9d03e571eef698251a5743351a9667fb4d7
Jeb Bearer <jeb.bearer@translucence.net>
2022-05-19 15:07:32 -0700

    Improvements to the faucet service
    * A background thread breaks large records into smaller ones,
      ensuring that the faucet wallet always has at least a given
      number of records at a time. This should help in processing
      simultaneous requests.
    * Faucet grants are distributed in multiple records, so that the
      wallet receiving the grant can easily use the records it receives
      to carry out simultaneous transactions.
    * The faucet waits until it has finished scanning the ledger before
      it starts serving requests.

--- faucet/src/faucet.rs
+++ faucet/src/faucet.rs
@@ -20,14 +20,18 @@ use cape_wallet::{
     wallet::{CapeWallet, CapeWalletError},
 };
 use ethers::prelude::Address;
+use futures::{channel::mpsc, SinkExt, StreamExt};
+use itertools::Itertools;
 use jf_cap::{
     keys::{UserKeyPair, UserPubKey},
-    structs::AssetCode,
+    structs::{AssetCode, FreezeFlag},
 };
 use rand::distributions::{Alphanumeric, DistString};
+use reef::traits::Validator;
 use seahorse::{
     events::EventIndex,
     loader::{Loader, LoaderMetadata},
+    txn_builder::{RecordInfo, TransactionStatus},
 };
 use serde::{Deserialize, Serialize};
 use snafu::Snafu;
@@ -68,12 +72,31 @@ pub struct FaucetOptions {
     pub faucet_port: String,
 
     /// size of transfer for faucet grant
-    #[structopt(long, env = "CAPE_FAUCET_GRANT_SIZE", default_value = "5000")]
+    #[structopt(long, env = "CAPE_FAUCET_GRANT_SIZE", default_value = "1000")]
     pub grant_size: u64,
 
+    /// number of grants to give out per request
+    #[structopt(long, env = "CAPE_FAUCET_NUM_GRANTS", default_value = "5")]
+    pub num_grants: usize,
+
     /// fee for faucet grant
-    #[structopt(long, env = "CAPE_FAUCET_FEE_SIZE", default_value = "100")]
+    #[structopt(long, env = "CAPE_FAUCET_FEE_SIZE", default_value = "0")]
     pub fee_size: u64,
+
+    /// number of records to maintain simultaneously.
+    ///
+    /// This allows N CAPE transfers to take place simultaneously. A reasonable value is the number
+    /// of simultaneous faucet requests you want to allow times CAPE_FAUCET_NUM_GRANTS. There is a
+    /// tradeoff in startup cost for having more simultaneous records: when the faucet initializes,
+    /// it must execute transfers to itself to break up its records into more, smaller ones. This
+    /// can take a long time, and it also forces the relayer to pay a lot of gas.
+    #[structopt(
+        long,
+        name = "N",
+        env = "CAPE_FAUCET_NUM_RECORDS",
+        default_value = "25"
+    )]
+    pub num_records: usize,
 
     /// URL for the Ethereum Query Service.
     #[structopt(long, env = "CAPE_EQS_URL", default_value = "http://localhost:50087")]
@@ -116,7 +139,33 @@ pub struct FaucetOptions {
 struct FaucetState {
     wallet: Arc<Mutex<CapeWallet<'static, CapeBackend<'static, LoaderMetadata>>>>,
     grant_size: u64,
+    num_grants: usize,
     fee_size: u64,
+    num_records: usize,
+    // Channel to signal when the distribution of records owned by the faucet changes. This will
+    // wake the record breaker thread (which waits on the receiver) so it can create more records by
+    // breaking up larger ones to maintain the target of `num_records`.
+    //
+    // We use a bounded channel so that a crashed or deadlocked record breaker thread that is not
+    // pulling messages out of the queue does not result in an unbounded memory leak.
+    signal_breaker_thread: mpsc::Sender<()>,
+}
+
+impl FaucetState {
+    pub fn new(
+        wallet: CapeWallet<'static, CapeBackend<'static, LoaderMetadata>>,
+        signal_breaker_thread: mpsc::Sender<()>,
+        opt: &FaucetOptions,
+    ) -> Self {
+        Self {
+            wallet: Arc::new(Mutex::new(wallet)),
+            grant_size: opt.grant_size,
+            num_grants: opt.num_grants,
+            fee_size: opt.fee_size,
+            num_records: opt.num_records,
+            signal_breaker_thread,
+        }
+    }
 }
 
 #[derive(Debug, Snafu, Serialize, Deserialize)]
@@ -170,27 +219,207 @@ async fn request_fee_assets(
     let pub_key: UserPubKey = net::server::request_body(&mut req).await?;
     let mut wallet = req.state().wallet.lock().await;
     let faucet_addr = wallet.pub_keys().await[0].address();
-    tracing::info!(
-        "transferring {} tokens from {} to {}",
-        req.state().grant_size,
-        net::UserAddress(faucet_addr.clone()),
-        net::UserAddress(pub_key.address())
-    );
-    let bal = wallet.balance(&AssetCode::native()).await;
-    tracing::info!("Wallet balance before transfer: {}", bal);
-    wallet
-        .transfer(
-            Some(&faucet_addr),
-            &AssetCode::native(),
-            &[(pub_key.address(), req.state().grant_size)],
-            req.state().fee_size,
-        )
-        .await
-        .map_err(|err| {
-            tracing::error!("Failed to transfer {}", err);
-            faucet_error(err)
-        })?;
-    net::server::response(&req, ())
+
+    let mut txns = Vec::new();
+    let mut errs = Vec::new();
+    for _ in 0..req.state().num_grants {
+        tracing::info!(
+            "transferring {} tokens from {} to {}",
+            req.state().grant_size,
+            net::UserAddress(faucet_addr.clone()),
+            net::UserAddress(pub_key.address())
+        );
+        let bal = wallet.balance(&AssetCode::native()).await;
+        tracing::info!("Wallet balance before transfer: {}", bal);
+        match wallet
+            .transfer(
+                Some(&faucet_addr),
+                &AssetCode::native(),
+                &[(pub_key.address(), req.state().grant_size)],
+                req.state().fee_size,
+            )
+            .await
+        {
+            Ok(receipt) => {
+                txns.push(receipt);
+            }
+            Err(err) => {
+                tracing::warn!("Failed to transfer: {}", err);
+                errs.push(err);
+            }
+        }
+    }
+
+    // Signal the record breaking thread that we have spent some records, so that it can create more
+    // by breaking up larger records. Drop our handle to the wallet (which we no longer need) so
+    // that the thread can access it.
+    drop(wallet);
+    if req
+        .state()
+        .signal_breaker_thread
+        .clone()
+        .try_send(())
+        .is_err()
+    {
+        tracing::error!("Error signalling the breaker thread. Perhaps it has crashed?");
+    }
+
+    if !txns.is_empty() {
+        // If we successfully transferred any assets, return success.
+        net::server::response(&req, txns)
+    } else {
+        // Otherwise, explain why the transactions failed.
+        let mut msgs = errs.into_iter().map(|err| format!("  - {}", err));
+        Err(faucet_server_error(FaucetError::Transfer {
+            msg: format!(
+                "All transfers failed for the following reasons:\n{}",
+                msgs.join("\n")
+            ),
+        }))
+    }
+}
+
+async fn spendable_records(
+    wallet: &CapeWallet<'static, CapeBackend<'static, LoaderMetadata>>,
+) -> impl Iterator<Item = RecordInfo> {
+    let now = wallet.lock().await.state().txn_state.validator.now();
+    wallet.records().await.filter(move |record| {
+        record.ro.asset_def.code == AssetCode::native()
+            && record.ro.freeze_flag == FreezeFlag::Unfrozen
+            && !record.on_hold(now)
+    })
+}
+
+/// Break large records into smaller records.
+///
+/// When signalled on `wakeup`, this thread will break large records into small records of size
+/// `state.grant_size`, until there are at least `state.num_records` distinct records in the wallet.
+async fn break_up_records(state: FaucetState, mut wakeup: mpsc::Receiver<()>) {
+    loop {
+        // Wait until we have few enough records that we need to break them up.
+        //
+        // This is a simulation of a condvar loop, since async condvar is unstable, hence the manual
+        // drop and reacquisition of the wallet mutex guard.
+        loop {
+            let wallet = state.wallet.lock().await;
+            let num_records = spendable_records(&*wallet).await.count();
+            if num_records < state.num_records {
+                break;
+            }
+
+            // We have enough records for now, wait for a signal that the number of records has
+            // changed.
+            tracing::info!(
+                "got {}/{} records, waiting for a change",
+                num_records,
+                state.num_records
+            );
+            drop(wallet);
+            wakeup.next().await;
+        }
+
+        // Start breaking up records until we have enough again.
+        loop {
+            // Acquire the wallet lock inside the loop, so we release it after each transfer.
+            // Holding the lock for too long can unneccessarily slow down faucet requests.
+            let mut wallet = state.wallet.lock().await;
+            let address = wallet.pub_keys().await[0].address();
+            let records = spendable_records(&*wallet).await.collect::<Vec<_>>();
+            if records.len() >= state.num_records {
+                // We have enough records again.
+                break;
+            }
+
+            // Find a record which can be broken down into two smaller `grant_size` records.
+            let record = match records
+                .into_iter()
+                .find(|record| record.ro.amount > 2 * state.grant_size)
+            {
+                Some(record) => record,
+                None => {
+                    // There are no big records to break up, so there's nothing for us to do. Exit
+                    // the inner loop and wait for a notification that the record distribution has
+                    // changed.
+                    tracing::warn!("not enough records, but no large records to break up");
+                    break;
+                }
+            };
+
+            tracing::info!(
+                "breaking up a record of size {} into records of size {} and {}",
+                record.ro.amount,
+                state.grant_size,
+                record.ro.amount - state.grant_size
+            );
+
+            // There is not yet an interface for transferring a specific record, so we just have to
+            // specify the appropriate amounts and trust that Seahorse will use the largest record
+            // available (it should). Just to be extra safe, we specify the larger of the two
+            // amounts -- record.ro.amount - state.grant_size -- as the output amount, which should
+            // create a change record of size `state.grant_size`. This makes it impossible for
+            // Seahorse to choose a record of exactly `state.grant_size` with no change, which would
+            // prevent this loop from making progress.
+            let receipt = match wallet
+                .transfer(
+                    None,
+                    &AssetCode::native(),
+                    &[(address.clone(), record.ro.amount - state.grant_size)],
+                    0,
+                )
+                .await
+            {
+                Ok(receipt) => receipt,
+                Err(err) => {
+                    // If our transfers start failing, we will assume there is something wrong and
+                    // try not to put extra stress on the system. Break out of the inner loop and
+                    // wait for a notification that something has changed.
+                    tracing::error!("record breakup transfer failed: {}", err);
+                    break;
+                }
+            };
+
+            // Wait for the transaction to complete so we get the change record before continuing.
+            match wallet.await_transaction(&receipt).await {
+                Ok(TransactionStatus::Retired) => continue,
+                _ => {
+                    tracing::error!("record breakup transfer did not complete successfully");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+/// Wait until the record breaker thread has created as many records as it can.
+///
+/// Repeatedly signals the thread until either `state.num_records` have been created, or there are
+/// no more big records to create. Returns the total number of records now in the wallet.
+async fn wait_for_records(state: &FaucetState) -> usize {
+    loop {
+        let wallet = state.wallet.lock().await;
+
+        // Break if we already have enough records, or if there are no big records to break up.
+        let num_records = spendable_records(&*wallet).await.count();
+        if num_records >= state.num_records
+            || !spendable_records(&*wallet)
+                .await
+                .any(|record| record.ro.amount > 2 * state.grant_size)
+        {
+            return num_records;
+        }
+
+        tracing::info!(
+            "have {} records, waiting for {} more",
+            num_records,
+            state.num_records - num_records
+        );
+
+        drop(wallet);
+        if state.signal_breaker_thread.clone().send(()).await.is_err() {
+            tracing::error!("Error signalling the breaker thread. Perhaps it has crashed?");
+            return num_records;
+        }
+    }
 }
 
 /// `faucet_key_pair` - If provided, will be added to the faucet wallet.
@@ -230,27 +459,43 @@ pub async fn init_web_server(
     // If a faucet key pair is provided, add it to the wallet. Otherwise, if we're initializing
     // for the first time, we need to generate a key. The faucet should be set up so that the
     // first HD sending key is the faucet key.
-    if let Some(key) = faucet_key_pair {
+    let new_key = if let Some(key) = faucet_key_pair {
         wallet
-            .add_user_key(key, "faucet".into(), EventIndex::default())
+            .add_user_key(key.clone(), "faucet".into(), EventIndex::default())
             .await
             .unwrap();
+        Some(key.pub_key())
     } else if wallet.pub_keys().await.is_empty() {
         // We pass `EventIndex::default()` to start a scan of the ledger from the beginning, in
-        // order to discove the faucet record.
-        wallet
-            .generate_user_key("faucet".into(), Some(EventIndex::default()))
-            .await
-            .unwrap();
+        // order to discover the faucet record.
+        Some(
+            wallet
+                .generate_user_key("faucet".into(), Some(EventIndex::default()))
+                .await
+                .unwrap(),
+        )
+    } else {
+        None
+    };
+    if let Some(key) = new_key {
+        // Wait until we have scanned the ledger for records belonging to this key.
+        wallet.await_key_scan(&key.address()).await.unwrap();
     }
 
     let bal = wallet.balance(&AssetCode::native()).await;
     tracing::info!("Wallet balance before init: {}", bal);
-    let state = FaucetState {
-        wallet: Arc::new(Mutex::new(wallet)),
-        grant_size: opt.grant_size,
-        fee_size: opt.fee_size,
-    };
+    // We use the total number of records to maintain as a conservative upper bound on how backed up
+    // the message channel can get.
+    let signal_breaker_thread = mpsc::channel(opt.num_records);
+    let state = FaucetState::new(wallet, signal_breaker_thread.0, opt);
+
+    // Spawn a thread to break records into smaller records to maintain `opt.num_records` at a time.
+    spawn(break_up_records(state.clone(), signal_breaker_thread.1));
+
+    // Wait for the thread to create at least `opt.num_records` if possible, before starting to
+    // handle requests.
+    wait_for_records(&state).await;
+
     let mut app = tide::with_state(state);
     app.with(
         CorsMiddleware::new()
@@ -283,14 +528,17 @@ async fn main() -> Result<(), std::io::Error> {
 #[cfg(test)]
 mod test {
     use super::*;
-    use cap_rust_sandbox::universal_param::UNIVERSAL_PARAM;
+    use cap_rust_sandbox::{ledger::CapeLedger, universal_param::UNIVERSAL_PARAM};
     use cape_wallet::testing::{create_test_network, retry, rpc_url_for_test, spawn_eqs};
+    use jf_cap::structs::AssetDefinition;
     use rand_chacha::{rand_core::SeedableRng, ChaChaRng};
-    use seahorse::hd::KeyTree;
+    use seahorse::{hd::KeyTree, txn_builder::TransactionReceipt};
     use std::path::PathBuf;
     use tempdir::TempDir;
+    use tracing_test::traced_test;
 
     #[async_std::test]
+    #[traced_test]
     async fn test_faucet_transfer() {
         let mut rng = ChaChaRng::from_seed([1u8; 32]);
         let universal_param = &*UNIVERSAL_PARAM;
@@ -308,14 +556,17 @@ mod test {
         // Initiate a faucet server with the mnemonic associated with the faucet key pair.
         let faucet_dir = TempDir::new("cape_wallet_faucet").unwrap();
         let faucet_port = "50079".to_string();
-        let grant_size = 5000;
+        let grant_size = 1000;
+        let num_grants = 5;
         let opt = FaucetOptions {
             mnemonic: mnemonic.to_string(),
             faucet_wallet_path: PathBuf::from(faucet_dir.path()),
             faucet_password: "".to_string(),
             faucet_port: faucet_port.clone(),
             grant_size,
-            fee_size: 100,
+            num_grants,
+            num_records: num_grants,
+            fee_size: 0,
             eqs_url: eqs_url.clone(),
             relayer_url: relayer_url.clone(),
             address_book_url: address_book_url.clone(),
@@ -357,7 +608,7 @@ mod test {
         println!("Receiver wallet created.");
 
         // Request native asset for the receiver.
-        surf::post(format!(
+        let mut response = surf::post(format!(
             "http://localhost:{}/request_fee_assets",
             faucet_port
         ))
@@ -367,7 +618,22 @@ mod test {
         .unwrap();
         println!("Asset transferred.");
 
+        let receipts: Vec<TransactionReceipt<CapeLedger>> = response.body_json().await.unwrap();
+        assert_eq!(receipts.len(), 5);
+
         // Check the balance.
-        retry(|| async { receiver.balance(&AssetCode::native()).await == grant_size }).await;
+        retry(|| async {
+            receiver.balance(&AssetCode::native()).await == grant_size * (num_grants as u64)
+        })
+        .await;
+
+        // We should have received `num_grants` records of `grant_size` each.
+        let records = receiver.records().await.collect::<Vec<_>>();
+        assert_eq!(records.len(), 5);
+        for record in records {
+            assert_eq!(record.ro.asset_def, AssetDefinition::native());
+            assert_eq!(record.ro.pub_key, receiver_key);
+            assert_eq!(record.ro.amount, grant_size);
+        }
     }
 }

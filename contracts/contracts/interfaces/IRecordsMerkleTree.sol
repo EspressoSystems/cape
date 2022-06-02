a0ca2b5528eed18b471addc8514a6f2d210ddaef
sveitser <sveitser@gmail.com>
2022-06-02 12:07:23 +0200

    Add missing file

---
+++ contracts/contracts/interfaces/IRecordsMerkleTree.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Copyright (c) 2022 Espresso Systems (espressosys.com)
+// This file is part of the Configurable Asset Privacy for Ethereum (CAPE) library.
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+interface IRecordsMerkleTree {
+    /// @param elements The list of elements to be appended to the current merkle tree described by the frontier.
+    function updateRecordsMerkleTree(uint256[] memory elements) external;
+
+    /// @notice Returns the root value of the Merkle tree.
+    function getRootValue() external view returns (uint256);
+
+    function getHeight() external view returns (uint8);
+
+    function getNumLeaves() external view returns (uint64);
+}

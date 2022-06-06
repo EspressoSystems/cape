dc0bad6901a7b6bb71b5fc2218ba71205b3e17ee
zhenfei <zhenfei.zhang@hotmail.com>
2022-06-06 15:43:58 -0400

    add bug disclaimer

--- contracts/contracts/verifier/Transcript.sol
+++ contracts/contracts/verifier/Transcript.sol
@@ -6,6 +6,10 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// There is currently a bug in solidity that may affect the security of the contract.
+// An patch is available for Jellyfish https://github.com/EspressoSystems/jellyfish/pull/70
+// Correponding changes are to be made to the solidity code here.
 
 pragma solidity ^0.8.0;
 

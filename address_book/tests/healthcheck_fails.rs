56bd073e7329ebb03ef7c70d325c4d31b7302ca9
sveitser <sveitser@gmail.com>
2022-06-13 18:22:05 +0200

    Test healthcheck without changing permissions
    Removing write permissions does not work on the CI.

--- address_book/tests/healthcheck_fails.rs
+++ address_book/tests/healthcheck_fails.rs
@@ -5,37 +5,21 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use std::{fs, time::Duration};
-
 use address_book::{
     address_book_port, address_book_temp_dir, init_web_server, wait_for_server, FileStore,
 };
 
+// This test has its own file because the address book port is currently not
+// configurable so we can't start more than one server concurrently.
 #[async_std::test]
-async fn test_healthcheck() {
+async fn test_healthcheck_fails() {
     let healthcheck_url = format!("http://127.0.0.1:{}/healthcheck", address_book_port());
 
-    let temp_dir = address_book_temp_dir();
-    let store = FileStore::new(temp_dir.path().to_path_buf());
+    let non_existent_temp_dir = address_book_temp_dir().path().join("dummy").to_path_buf();
+    let store = FileStore::new(non_existent_temp_dir);
     init_web_server(store).await.expect("Failed to run server.");
     wait_for_server().await;
-
-    // Test healtheck OK
     let response = surf::get(&healthcheck_url).await.unwrap();
-    assert_eq!(response.status(), tide::StatusCode::Ok);
-
-    // Test healthcheck fails if the directory isn't writeable.
-    let mut permissions = temp_dir.path().metadata().unwrap().permissions();
-    permissions.set_readonly(true);
-    fs::set_permissions(temp_dir.path(), permissions.clone()).unwrap();
-    std::thread::sleep(Duration::from_secs(1));
-
-    let response = surf::get(&healthcheck_url).await.unwrap();
-
-    // Make directory writeable again now so it can be removed if the assert
-    // below fails.
-    permissions.set_readonly(false);
-    fs::set_permissions(temp_dir.path(), permissions).unwrap();
 
     assert_eq!(response.status(), tide::StatusCode::InternalServerError);
 }
